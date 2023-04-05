1c891443a93bd24a186b06000e835ee94e062ae1
D.J. Figueroa <djfigs10@gmail.com>
2023-04-04 23:23:19 -0400

    Change to TypeScript

---
+++ src/Home.tsx
@@ -0,0 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export const Home: React.FC = ({}) => {
+    return <View>
+        <Text style={{fontSize: 100, color: "red"}}>The future is now!</Text>
+    </View>
+}
